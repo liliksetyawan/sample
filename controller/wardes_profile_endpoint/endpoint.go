f7e39d6bc0acef17e1ffd8ad6c4fd7d8203d2524
liliksetyawan <124754975+liliksetyawan@users.noreply.github.com>
2025-10-10 14:17:51 +0700

    update

---
+++ controller/wardes_profile_endpoint/endpoint.go
@@ -0,0 +1,74 @@
+package wardes_profile_endpoint
+
+import(
+	"fmt"
+	"net/http"
+	http_validator "github.com/nexsoft-git/nexcommon/controller/http"
+	"nexsoft.co.id/example/config"
+	wardes_profileService "nexsoft.co.id/example/service/wardes_profile"
+)
+
+func NewWardesProfileEndpoint(
+	httpValidator *http_validator.HTTPController,
+	srv wardes_profileService.WardesProfileService,
+	config config.Configuration,
+) *wardesProfileEndpoint {
+	return &wardesProfileEndpoint{
+		httpValidator   : httpValidator,
+		srv             : srv,
+		config          : config,
+	}
+}
+
+type wardesProfileEndpoint struct {
+	httpValidator *http_validator.HTTPController
+	srv           wardes_profileService.WardesProfileService
+	config        config.Configuration
+}
+
+func (e *wardesProfileEndpoint) RegisterEndpoint() { 
+
+	e.httpValidator.HandleFunc(
+		http_validator.NewHandleFuncParam(
+		    fmt.Sprintf("/v1/%s/wardes_profile", e.config.Server.ResourceID),
+			e.httpValidator.WrapService(
+				http_validator.NewWarpServiceParam(
+					e.srv,
+					e.srv.InsertWardesProfile,
+					e.httpValidator.UserAccessValidatorWithKong,
+				).Menu("insert").
+				Permission("master.wardes_profile.wardes_profile:insert"),
+			),
+			http.MethodPost, http.MethodOptions,
+		),
+	)
+
+	e.httpValidator.HandleFunc(
+		http_validator.NewHandleFuncParam(
+			fmt.Sprintf("/v1/%s/wardes_profile", e.config.Server.ResourceID),
+			e.httpValidator.WrapServiceListData(
+				http_validator.NewWarpGetListServiceParam(
+					e.srv,
+					e.srv.GetWardesProfileList,
+					e.httpValidator.UserAccessValidatorWithKong,
+				).Permisson("master.wardes_profile.wardes_profile:view"),
+			),
+			http.MethodGet, http.MethodOptions,
+		),
+	)
+
+	e.httpValidator.HandleFunc(
+		http_validator.NewHandleFuncParam(
+		    fmt.Sprintf("/v1/%s/wardes_profile/count", e.config.Server.ResourceID),
+			e.httpValidator.WrapService(
+				http_validator.NewWarpServiceParam(
+					e.srv,
+					e.srv.GetWardesProfileCount,
+					e.httpValidator.UserAccessValidatorWithKong,
+				).Menu("count").
+				Permission("master.wardes_profile.wardes_profile:view"),
+			),
+			http.MethodGet, http.MethodOptions,
+		),
+	)
+}
